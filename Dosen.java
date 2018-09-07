b2912abb4edf7e95e540b053a10a83e45f73f550
Saffana Dira <zheliaalifa@gmail.com>
2018-09-07 16:35:02 +0700

    Add Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,27 @@
+public class Dosen {
+	String name;
+	String age;
+
+	public Dosen (String name, int age) {
+		super();
+		this.name = name;
+		this.npm = npm;
+		this.age = age;
+	}
+
+	public getName() {
+		return name;
+	}
+
+	public void setName(String dosenName) {
+		name = dosenName;
+	}
+
+	public getAge() {
+		return age;
+	}
+
+	public void setAge(int dosenAge) {
+		age = dosenAge;
+	}
+}
